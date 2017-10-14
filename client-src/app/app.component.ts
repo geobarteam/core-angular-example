c9661af9bcad972091febc0b5a1d49f7a33c1f56
geobarteam <gvd8@hotmail.com>
2017-10-14 11:30:18 +0200

    Configure csproj for release build

--- client-src/app/app.component.ts
+++ client-src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Http } from '@angular/http'
 })
 export class AppComponent implements OnInit {
    constructor(private _httpService: Http) { }
+   title = "DotNet Core Angular Example";
    apiValues: string[] = [];
    ngOnInit() {
       this._httpService.get('/api/values').subscribe(values => {
